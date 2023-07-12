42c382690556b8e2564778c9e6bb1e3cd2cd71b2
FabrizioFavia <personal.favia@gmail.com>
2023-07-12 17:55:16 +0200

    fixed bug(pivot table)

--- database/migrations/2023_07_12_155300_create_project_technology_table.php
+++ database/migrations/2023_07_12_155300_create_project_technology_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('technology_project', function (Blueprint $table) {
+        Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
-
             $table->foreignId('project_id')->nullable()->constrained(); 
             $table->foreignId('technology_id')->nullable()->constrained();
-
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('technology_project');
+        Schema::dropIfExists('project_technology');
     }
 };
